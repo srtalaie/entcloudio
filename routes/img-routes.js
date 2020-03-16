a79cbcfdc535bbc38aae0725e6af4d4b555b9af6
srtalaie <srt4721@yahoo.com>
2020-03-16 15:01:01 -0700

    added features row and pics

--- routes/img-routes.js
+++ routes/img-routes.js
@@ -5,5 +5,24 @@ module.exports = function(app){
     app.get('/images/hero-img', function(req, res){
         res.sendFile(path.join(__dirname, "../public/assets/images/hero.jpg"));
     });
+    
+    app.get('/images/playstorebadge', function(req, res){
+        res.sendFile(path.join(__dirname, "../public/assets/images/playstorebadge.png"));
+    });
 
+    app.get('/images/web', function(req, res){
+        res.sendFile(path.join(__dirname, "../public/assets/images/web.png"));
+    });
+
+    app.get('/images/encrypted', function(req, res){
+        res.sendFile(path.join(__dirname, "../public/assets/images/encrypted.svg"));
+    });
+
+    app.get('/images/independent', function(req, res){
+        res.sendFile(path.join(__dirname, "../public/assets/images/independent.svg"));
+    });
+
+    app.get('/images/wifi', function(req, res){
+        res.sendFile(path.join(__dirname, "../public/assets/images/wifi.svg"));
+    });
 }
